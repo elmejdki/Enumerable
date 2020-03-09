d4de88ea3499b2ca2c65d611ad723aa77a7d50af
Zakariae El Mejdki <elmejdki.zakariae@gmail.com>
2020-03-09 19:45:08 +0100

    Add my_each method into the Enumerable module

--- main.rb
+++ main.rb
@@ -1,3 +1,19 @@
-# rubocop:disable Style/CaseEquality
+# rubocop:disable Metrics/ModuleLength
+# rubocop:disable Metrics/MethodLength
+# rubocop:disable Metrics/CyclomaticComplexity
+# rubocop:disable Metrics/PerceivedComplexity
+module Enumerable
+  def my_each
+    for i in 0..length - 1 do
+      yield(self[i])
+    end
 
-# rubocop:enable Style/CaseEquality
+    self
+  end
+end
+# rubocop:enable Metrics/ModuleLength
+# rubocop:enable Metrics/MethodLength
+# rubocop:enable Metrics/CyclomaticComplexity
+# rubocop:enable Metrics/PerceivedComplexity
+
+%w[gogo toto boto].my_each { |x| puts x + "." }
